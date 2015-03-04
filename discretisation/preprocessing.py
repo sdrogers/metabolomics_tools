a375ae951d5eadcab62a5a1ceae7139ed0419990
Joe Wandy <joewandy@gmail.com>
2015-03-04 18:13:11 +0000

    tidy up messy code
    Former-commit-id: b019fc72f0b3c241dfdc5c9923aee6f372952823

--- discretisation/preprocessing.py
+++ discretisation/preprocessing.py
@@ -17,9 +17,7 @@ class Discretiser(object):
         self.mass_tol = mass_tol
         self.rt_tol = rt_tol
     
-    def run(self, current_features, new_features, binning):       
-
-        ### TODO: lots of code duplication in this method ..
+    def run(self, features):       
 
         adduct_name = np.array([t.name for t in self.transformations])[:,None]      # A x 1
         adduct_mul = np.array([t.mul for t in self.transformations])[:,None]        # A x 1
@@ -29,107 +27,43 @@ class Discretiser(object):
         # find index of M+H adduct in the list of transformations
         proton_pos = np.flatnonzero(np.array(adduct_name)=='M+H') 
 
-        # if no binning information, i.e. first file
-        if binning is None:
-
-            # then make bins using all the features in the file
-            N = len(new_features)
-            K = N
-            feature_masses = np.array([f.mass for f in new_features])[:, None]              # N x 1
-            prior_rts = np.array([f.rt for f in new_features])[:, None]                     # N x 1
-            prior_intensities = np.array([f.intensity for f in new_features])[:, None]      # N x 1
-
-            prior_masses = (feature_masses - adduct_sub[proton_pos])/adduct_mul[proton_pos] # K x 1
-            matRT = sp.lil_matrix((N, K), dtype=np.float)                                   # N x K, RTs of feature n in bin k
-            possible = sp.lil_matrix((N, K), dtype=np.int)                                  # N x K, transformation id+1 of feature n in bin k
-            transformed = sp.lil_matrix((N, K), dtype=np.float)                             # N x K, transformed masses of feature n in bin k
-            bins = []
-            
-            # populate possible, transformed, matRT
-            for n in range(N):
-                
-                if n%100 == 0:
-                    print '.',
-    
-                current_mass, current_rt, current_intensity = feature_masses[n], prior_rts[n], prior_intensities[n]
-                pc_bin = self._make_precursor_bin(n, prior_masses[n], current_rt, current_intensity, self.mass_tol, self.rt_tol)
-                bins.append(pc_bin)
-                
-                prior_mass = (current_mass - adduct_sub)/adduct_mul + adduct_del
-                rt_ok = utils.rt_match(current_rt, prior_rts, self.rt_tol)
-                intensity_ok = (current_intensity <= prior_intensities)
-                for t in np.arange(len(self.transformations)):
-                    mass_ok = utils.mass_match(prior_mass[t], prior_masses, self.mass_tol)
-                    pos = np.flatnonzero(rt_ok*mass_ok*intensity_ok)
-                    possible[n, pos] = t+1
-                    transformed[n, pos] = prior_mass[t]
-                    matRT[n, pos] = current_rt            
-
-            print
-            print "Total bins=" + str(K) + " total features=" + str(N)
-            binning = DiscreteInfo(possible, transformed, matRT, bins, prior_masses, prior_rts)
-            return binning         
+        # make bins using all the features in the file
+        N = len(features)
+        K = N # by definition
+        feature_masses = np.array([f.mass for f in features])[:, None]              # N x 1
+        prior_rts = np.array([f.rt for f in features])[:, None]                     # K x 1
+        prior_intensities = np.array([f.intensity for f in features])[:, None]      # K x 1
+        prior_masses = (feature_masses - adduct_sub[proton_pos])/adduct_mul[proton_pos] # K x 1
+        matRT = sp.lil_matrix((N, K), dtype=np.float)                                   # N x K, RTs of feature n in bin k
+        possible = sp.lil_matrix((N, K), dtype=np.int)                                  # N x K, transformation id+1 of feature n in bin k
+        transformed = sp.lil_matrix((N, K), dtype=np.float)                             # N x K, transformed masses of feature n in bin k
+        bins = []
         
-        else: # otherwise need to do some checking ...
-        
-            # we want to make new bins only for features that cannot go into any existing bins from the previous files
-            bins = binning.bins
-            prior_masses = np.array([b.mass for b in bins])[:, None]
-            prior_rts = np.array([b.rt for b in bins])[:, None]
-            prior_intensities = np.array([b.intensity for b in bins])[:, None]
-            max_bin_id = np.array([b.bin_id for b in bins]).max()
+        # populate possible, transformed, matRT
+        for n in range(N):
             
-            # for each potential new cluster ..
-            for can in new_features:
-                
-                precursor_mass = (can.mass - adduct_sub[proton_pos])/adduct_mul[proton_pos] 
-                mass_ok = utils.mass_match(precursor_mass, prior_masses, self.mass_tol)
-                rt_ok = utils.rt_match(can.rt, prior_rts, self.rt_tol)
-                intensity_ok = (can.intensity <= prior_intensities)
-                check = rt_ok*mass_ok*intensity_ok
-                
-                # if no suitable existing bin found
-                if check.sum(0)==0:
-                    # then make new bin
-                    max_bin_id += 1
-                    pc_bin = self._make_precursor_bin(max_bin_id, precursor_mass, can.rt, can.intensity, 
-                                                      self.mass_tol, self.rt_tol)
-                    bins.append(pc_bin)
+            if n%100 == 0:
+                print '.',
 
-            # rebuild the matrices
-            prior_masses = np.array([b.mass for b in bins])[:, None]            # K x 1
-            prior_rts = np.array([b.rt for b in bins])[:, None]                 # K x 1
-            prior_intensities = np.array([b.intensity for b in bins])[:, None]  # K x 1
+            current_mass, current_rt, current_intensity = feature_masses[n], prior_rts[n], prior_intensities[n]
+            pc_bin = self._make_precursor_bin(n, prior_masses[n], current_rt, current_intensity, self.mass_tol, self.rt_tol)
+            bins.append(pc_bin)
+            
+            prior_mass = (current_mass - adduct_sub)/adduct_mul + adduct_del
+            rt_ok = utils.rt_match(current_rt, prior_rts, self.rt_tol)
+            intensity_ok = (current_intensity <= prior_intensities)
+            for t in np.arange(len(self.transformations)):
+                mass_ok = utils.mass_match(prior_mass[t], prior_masses, self.mass_tol)
+                pos = np.flatnonzero(rt_ok*mass_ok*intensity_ok)
+                possible[n, pos] = t+1
+                transformed[n, pos] = prior_mass[t]
+                matRT[n, pos] = current_rt            
 
-            features = list(current_features)
-            features.extend(new_features)
-            N = len(features)
-            K = len(bins)
-
-            matRT = sp.lil_matrix((N, K), dtype=np.float)       # N x K, RTs of feature n in bin k
-            possible = sp.lil_matrix((N, K), dtype=np.int)      # N x K, transformation id+1 of feature n in bin k
-            transformed = sp.lil_matrix((N, K), dtype=np.float) # N x K, transformed masses of feature n in bin k
-
-            # populate possible, transformed, matRT
-            for n in range(N):
-    
-                f = features[n]    
-                current_mass, current_rt, current_intensity = f.mass, f.rt, f.intensity
-                
-                prior_mass = (current_mass - adduct_sub)/adduct_mul + adduct_del
-                rt_ok = utils.rt_match(current_rt, prior_rts, self.rt_tol)
-                intensity_ok = (current_intensity <= prior_intensities)
-                for t in np.arange(len(self.transformations)):
-                    mass_ok = utils.mass_match(prior_mass[t], prior_masses, self.mass_tol)
-                    pos = np.flatnonzero(rt_ok*mass_ok*intensity_ok)
-                    possible[n, pos] = t+1
-                    transformed[n, pos] = prior_mass[t]
-                    matRT[n, pos] = current_rt            
-                            
-            print "Total bins=" + str(K) + " total features=" + str(N)
-            binning = DiscreteInfo(possible, transformed, matRT, bins, prior_masses, prior_rts)
-            return binning         
-    
+        print
+        print "Total bins=" + str(K) + " total features=" + str(N)
+        binning = DiscreteInfo(possible, transformed, matRT, bins, prior_masses, prior_rts)
+        return binning         
+            
     def _make_precursor_bin(self, bin_id, bin_mass, bin_RT, bin_intensity, mass_tol, rt_tol):
         bin_mass = utils.as_scalar(bin_mass)
         bin_RT = utils.as_scalar(bin_RT)
@@ -160,22 +94,20 @@ class FileLoader:
             filelist = utils.natural_sort(filelist)
             
             # process file one by one
-            features = []
-            binning = None
             file_id = 0
+            data_list = []
             for file_path in filelist:
-                # file_path = os.path.abspath(file_path)
-                new_features = self.load_features(file_path, synthetic=synthetic)
-                for f in new_features:
+                features = self.load_features(file_path, synthetic=synthetic)
+                for f in features:
                     f.file_id = file_id
-                print "Processing file_id=" + str(file_id) + " " + file_path + " " + str(len(new_features)) + " features"
+                print "Processing file_id=" + str(file_id) + " " + file_path + " " + str(len(features)) + " features"
                 file_id += 1
                 if make_bins:
                     discretiser = Discretiser(transformations, mass_tol, rt_tol)
-                    binning = discretiser.run(features, new_features, binning)
-                features.extend(new_features)
-            data = PeakData(features, database, transformations, binning)
-            return data
+                    binning = discretiser.run(features)
+                data = PeakData(features, database, transformations, binning)
+                data_list.append(data)
+            return data_list
                     
         else:            
             # process only a single file
@@ -183,7 +115,7 @@ class FileLoader:
             binning = None
             if make_bins:
                 discretiser = Discretiser(transformations, mass_tol, rt_tol)
-                binning = discretiser.run([], features, None)
+                binning = discretiser.run(features)
             data = PeakData(features, database, transformations, binning)
             return data
         
