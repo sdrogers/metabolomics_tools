e48eb9cbd9c54ebd70b51a3dbf7a1203edd8e31e
Joe Wandy <joewandy@gmail.com>
2015-09-07 19:18:43 +0100

    finished fragment words extraction for multiple input files
    Former-commit-id: eb6363c6e7041d659f81ca8b283866ac025de76d

--- multifile_lda/R/extractFragmentFeatures.R
+++ multifile_lda/R/extractFragmentFeatures.R
@@ -1,47 +1,28 @@
-extract_ms2_fragment_df <- function(ms1, ms2, config) {
-  
-    prev_words_file <- config$input_files$previous_words_file
-    grouping_tol <- config$MS1MS2_matrixGeneration_parameters$grouping_tol_frags
-  
-    ########################################
-    ##### MS1/MS2 Dataframe Generation #####
-    ########################################
-    
-    print("Constructing MS1/MS2 dataframe")
-    
+extract_ms2_fragment_df_single <- function(ms1, ms2, grouping_tol, common_bins) {
+            
     # create an empty dataframe for existing words
     existing_fragment_df <- data.frame(t(rep(NA,length(ms1$peakID))))
     existing_fragment_df <- existing_fragment_df[-1,] # remove first column
-
+    
     # create an empty dataframe for new words
     new_fragment_df <- data.frame(t(rep(NA,length(ms1$peakID))))
     new_fragment_df <- new_fragment_df[-1,] # remove first column
     
     ms1.names <- as.character(ms1$peakID) # set row names on ms1 dataframe
     ms2.names <- as.character(ms2$peakID) # set row names on ms2 dataframe
-    
-    # reuse existing words, if any
-    copy_ms2 <- ms2
-    if (file.exists(prev_words_file)) {
 
-        prev_words <- read.csv(prev_words_file, header=F)
-        pos <-with(prev_words, grepl("fragment", V1))
+    copy_ms2 <- ms2        
+    common_bins_len <- length(common_bins)        
+    for (i in 1:length(common_bins)) {
         
-        # select the rows containing 'fragment' and make it into a vector for strsplit
-        prev_words <- as.vector(prev_words[pos, 1]) 
+        mz <- common_bins[i]
+        print(paste(c("i=", i, "/", common_bins_len, 
+                      ", mz=", mz,
+                      ", remaining=", nrow(copy_ms2)), collapse=""))
         
-        # split by _
-        tokens <- strsplit(prev_words, '_')
-        prev_mzs <- sapply(tokens, '[', 2)
-        prev_mzs <- as.numeric(prev_mzs)
-        prev_mzs_len <- length(prev_mzs)
+        word_found <- FALSE
+        if (nrow(copy_ms2) > 0) {
         
-        for (i in 1:prev_mzs_len) {
-
-            print(paste(c("i=", i, "/", prev_mzs_len, 
-                          ", remaining=", nrow(copy_ms2)), collapse=""))
-            mz <- prev_mzs[i]
-                        
             # calculate mz window
             max.ppm <- mz * grouping_tol * 1e-06
             
@@ -49,50 +30,80 @@ extract_ms2_fragment_df <- function(ms1, ms2, config) {
             match.idx <- which(sapply(copy_ms2$mz, function(x) {
                 abs(mz - x) < max.ppm
             }))    
+                    
+            if (length(match.idx)>0) {
+                word_found <- TRUE
+            }
             
-            # use the existing word as label for the rows
-            mean.mz <- round(mz, digits=5)
-
-            # if there's a match then use the actual fragment peaks
-            if (length(match.idx)>0) { 
-            
-                # store the mean mz (bin id) into the original ms2 dataframe too
-                peakids <- copy_ms2$peakID[match.idx]
-                matching_pos <- match(as.character(peakids), ms2.names)
-                ms2[matching_pos, "fragment_bin_id"] <- as.character(mean.mz)
-                
-                # get intensities
-                intensities <- copy_ms2$intensity[match.idx]
-                
-                # get parent id
-                parent.id <- copy_ms2$MSnParentPeakID[match.idx]
-                
-                # add a row of the intensities of the fragments
-                parent.idx <- match(as.character(parent.id), ms1.names)
-                row <- rep(NA, nrow(ms1))
-                row[parent.idx] <- intensities                
-                existing_fragment_df <- rbind(existing_fragment_df, row)
-                rownames(existing_fragment_df)[nrow(existing_fragment_df)] <- paste(c("fragment_", mean.mz), collapse="")
-                
-                # remove fragments from ms2 list and start loop again with next fragment
-                copy_ms2 <- copy_ms2[-match.idx,]
-                            
-            } else { # otherwise just insert a row of all NAs   
-            
-                row <- rep(NA, nrow(ms1))
-                existing_fragment_df <- rbind(existing_fragment_df, row)
-                rownames(existing_fragment_df)[nrow(existing_fragment_df)] <- paste(c("fragment_", mean.mz), collapse="")
-            
-            }            
-                                    
         }
 
+        mean.mz <- round(mz, digits=5) # the bin label
+        
+        # if there's a match then use the actual fragment peaks
+        if (word_found) { 
+            
+            # store the mean mz (bin id) into the original ms2 dataframe too
+            peakids <- copy_ms2$peakID[match.idx]
+            matching_pos <- match(as.character(peakids), ms2.names)
+            ms2[matching_pos, "fragment_bin_id"] <- as.character(mean.mz)
+            
+            # get intensities
+            intensities <- copy_ms2$intensity[match.idx]
+            
+            # get parent id
+            parent.id <- copy_ms2$MSnParentPeakID[match.idx]
+            
+            # add a row of the intensities of the fragments
+            parent.idx <- match(as.character(parent.id), ms1.names)
+            row <- rep(NA, nrow(ms1))
+            row[parent.idx] <- intensities                
+            existing_fragment_df <- rbind(existing_fragment_df, row)
+            rownames(existing_fragment_df)[nrow(existing_fragment_df)] <- paste(c("fragment_", mean.mz), collapse="")
+            
+            # remove fragments from ms2 list and start loop again with next fragment
+            copy_ms2 <- copy_ms2[-match.idx,]
+            
+        } else { # otherwise just insert a row of all NAs   
+            
+            row <- rep(NA, nrow(ms1))
+            existing_fragment_df <- rbind(existing_fragment_df, row)
+            rownames(existing_fragment_df)[nrow(existing_fragment_df)] <- paste(c("fragment_", mean.mz), collapse="")
+            
+        }            
+        
     }
     
-    # then process the remaining fragments that have not been discretised yet
+    # all ms2 must be binned
+    stopifnot(nrow(copy_ms2) == 0)  
+    
+    existing_fragment_df <- existing_fragment_df[mixedsort(row.names(existing_fragment_df)),]
+    names(existing_fragment_df) <- paste(as.character(round(ms1$mz, digits=5)), 
+                                         as.character(ms1$rt),
+                                         as.character(ms1$peakID),
+                                         sep="_")
+    
+    new_fragment_df <- new_fragment_df[mixedsort(row.names(new_fragment_df)),]
+    names(new_fragment_df) <- paste(as.character(round(ms1$mz, digits=5)), 
+                                    as.character(ms1$rt),
+                                    as.character(ms1$peakID),
+                                    sep="_")
+    
+    fragment_df <- rbind(existing_fragment_df, new_fragment_df)    
+    
+    # ms2 has been modified and needs to be returned too
+    output <- list("fragment_df"=fragment_df, "ms2"=ms2)
+    return(output)
+    
+}
+
+get_common_fragments <- function(combined_ms2, grouping_tol) {
+
+    copy_ms2 <- combined_ms2    
+    copy_ms2 <- copy_ms2[with(copy_ms2, order(mz)), ]    
+    common_bins <- vector()    
     while(nrow(copy_ms2) > 0) {
         
-        print(paste(c("remaining=", nrow(copy_ms2)), collapse=""))
+        print(paste(c("binning=", nrow(copy_ms2)), collapse=""))
         
         # get first mz value
         mz <- copy_ms2$mz[1]
@@ -106,47 +117,40 @@ extract_ms2_fragment_df <- function(ms1, ms2, config) {
         }))    
         
         # calculate mean mz as label for ms2 row
-        mean.mz <- round(mean(copy_ms2$mz[match.idx]), digits=5)
-        
-        # store the mean mz (bin id) into the original ms2 dataframe too
-        peakids <- copy_ms2$peakID[match.idx]
-        matching_pos <- match(as.character(peakids), ms2.names)
-        ms2[matching_pos, "fragment_bin_id"] <- as.character(mean.mz)
-        
-        # get intensities
-        intensities <- copy_ms2$intensity[match.idx]
-        
-        # get parent id
-        parent.id <- copy_ms2$MSnParentPeakID[match.idx]
-        
-        # find parent id in data.frame and add ms2 fragments
-        parent.idx <- match(as.character(parent.id), ms1.names)
-        row <- rep(NA, nrow(ms1))
-        row[parent.idx] <- intensities
-        new_fragment_df <- rbind(new_fragment_df, row)
-        rownames(new_fragment_df)[nrow(new_fragment_df)] <- paste(c("fragment_", mean.mz), collapse="")
-        
+        mean.mz <- mean(copy_ms2$mz[match.idx])
+        common_bins <- c(common_bins, mean.mz)
+                
         # remove fragments from ms2 list and start loop again with next fragment
         copy_ms2 <- copy_ms2[-match.idx,]
         
     }
     
-    existing_fragment_df <- existing_fragment_df[mixedsort(row.names(existing_fragment_df)),]
-    names(existing_fragment_df) <- paste(as.character(round(ms1$mz, digits=5)), 
-                                as.character(ms1$rt),
-                                as.character(ms1$peakID),
-                                sep="_")
-        
-    new_fragment_df <- new_fragment_df[mixedsort(row.names(new_fragment_df)),]
-    names(new_fragment_df) <- paste(as.character(round(ms1$mz, digits=5)), 
-                                         as.character(ms1$rt),
-                                         as.character(ms1$peakID),
-                                         sep="_")
+    return(common_bins)
     
-    fragment_df <- rbind(existing_fragment_df, new_fragment_df)    
+}
+
+extract_ms2_fragment_df <- function(all_ms1, all_ms2, config) {
+
+    stopifnot(length(all_ms1)==length(all_ms2))
+    
+    print("Constructing common fragment bins shared across files")
+    
+    grouping_tol <- config$MS1MS2_matrixGeneration_parameters$grouping_tol_frags  
+    combined_ms2 <- do.call('rbind', all_ms2)
+    common_bins <- get_common_fragments(combined_ms2, grouping_tol)
+    
+    fragment_matrices <- list()
+    for (i in 1:length(all_ms2)) {
+
+        print(paste(c("Constructing fragment matrix for file", i), collapse=" "))
+        file_ms1 <- all_ms1[[i]]
+        file_ms2 <- all_ms2[[i]]
+        file_results <- extract_ms2_fragment_df_single(file_ms1, file_ms2, grouping_tol, common_bins)
+        fragment_matrices[[i]] <- file_results$fragment_df
+        all_ms2[[i]] <- file_results$ms2
         
-    # ms2 has been modified and needs to be returned too
-    output <- list("fragment_df"=fragment_df, "ms2"=ms2)
-    return(output)
+    }
+    all_results <- list("fragment_df"=fragment_matrices, "ms2"=all_ms2)
+    return(all_results)
     
 }
--- multifile_lda/R/runCreatePeakMethod1.R
+++ multifile_lda/R/runCreatePeakMethod1.R
@@ -2,28 +2,37 @@
 
 source('xcmsSetFragments.R')
 
+run_create_peak_method_1_single <- function(input_file, config) {
+
+    print(paste(c("Processing ", input_file), collapse=""))
+    
+    # do peak detection using CentWave
+    xset <- xcmsSet(files=input_file, method=config$MS1_XCMS_peakpicking_settings$method, ppm=config$MS1_XCMS_peakpicking_settings$ppm, snthresh=config$MS1_XCMS_peakpicking_settings$snthres, peakwidth=c(config$MS1_XCMS_peakpicking_settings$peakwidth_from,config$MS1_XCMS_peakpicking_settings$peakwidth_to),
+                    prefilter=c(config$MS1_XCMS_peakpicking_settings$prefilter_from,config$MS1_XCMS_peakpicking_settings$prefilter_to), mzdiff=config$MS1_XCMS_peakpicking_settings$mzdiff, integrate=config$MS1_XCMS_peakpicking_settings$integrate, fitgauss=config$MS1_XCMS_peakpicking_settings$fitgauss, 
+                    verbose.column=config$MS1_XCMS_peakpicking_settings$verbose.column,
+                    nSlaves=4)
+    xset <- group(xset)
+    
+    # run Tony's script
+    frags <- xcmsSetFragments(xset, cdf.corrected = config$ms1_ms2_pairing_parameters$cdf.corrected, min.rel.int=config$filtering_parameters_MassSpectrometry_related$min.rel.int, max.frags = config$filtering_parameters_MassSpectrometry_related$mass.frags, 
+                              msnSelect=c(config$ms1_ms2_pairing_parameters$msnSelect), specFilter=c(config$ms1_ms2_pairing_parameters$specFilter), match.ppm=config$ms1_ms2_pairing_parameters$match.ppm, 
+                              sn=config$filtering_parameters_MassSpectrometry_related$sn, mzgap=config$filtering_parameters_MassSpectrometry_related$mz_gap, min.r=config$ms1_ms2_pairing_parameters$min.r, min.diff=config$ms1_ms2_pairing_parameters$min.diff)
+    peaks <- as.data.frame(frags@peaks)
+    
+    return(peaks)
+    
+}
+
 run_create_peak_method_1 <- function(config) {
   
     input_dir <- config$input_files$fragmentation_file_m1
     files <- list.files(path=input_dir, pattern="*.mzXML", full.names=T, recursive=FALSE)
-    results <- lapply(files, function(input_file) {
-        
-        print(paste(c("Processing ", input_file), collapse=""))
-
-        # do peak detection using CentWave
-        xset <- xcmsSet(files=input_file, method=config$MS1_XCMS_peakpicking_settings$method, ppm=config$MS1_XCMS_peakpicking_settings$ppm, snthresh=config$MS1_XCMS_peakpicking_settings$snthres, peakwidth=c(config$MS1_XCMS_peakpicking_settings$peakwidth_from,config$MS1_XCMS_peakpicking_settings$peakwidth_to),
-                        prefilter=c(config$MS1_XCMS_peakpicking_settings$prefilter_from,config$MS1_XCMS_peakpicking_settings$prefilter_to), mzdiff=config$MS1_XCMS_peakpicking_settings$mzdiff, integrate=config$MS1_XCMS_peakpicking_settings$integrate, fitgauss=config$MS1_XCMS_peakpicking_settings$fitgauss, verbose.column=config$MS1_XCMS_peakpicking_settings$verbose.column)
-        xset <- group(xset)
-        
-        # run Tony's script
-        frags <- xcmsSetFragments(xset, cdf.corrected = config$ms1_ms2_pairing_parameters$cdf.corrected, min.rel.int=config$filtering_parameters_MassSpectrometry_related$min.rel.int, max.frags = config$filtering_parameters_MassSpectrometry_related$mass.frags, 
-                                  msnSelect=c(config$ms1_ms2_pairing_parameters$msnSelect), specFilter=c(config$ms1_ms2_pairing_parameters$specFilter), match.ppm=config$ms1_ms2_pairing_parameters$match.ppm, 
-                                  sn=config$filtering_parameters_MassSpectrometry_related$sn, mzgap=config$filtering_parameters_MassSpectrometry_related$mz_gap, min.r=config$ms1_ms2_pairing_parameters$min.r, min.diff=config$ms1_ms2_pairing_parameters$min.diff)
-        peaks <- as.data.frame(frags@peaks)
-        
-        return(peaks)
-
-    })
+    results <- list()
+    for (i in 1:length(files)) {
+        input_file <- files[i]
+        res <- run_create_peak_method_1_single(input_file, config)
+        results[[length(results)+1]] <- res
+    }
     return(results)
-    
+       
 }
