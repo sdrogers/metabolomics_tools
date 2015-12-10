d7df6baa81abf724bef94a3cfc62faabb481eb5c
Simon Rogers <simon.rogers@glasgow.ac.uk>
2015-12-10 21:19:07 +0000

    simon playing with ms1 spectra clustering
    Former-commit-id: e48e924b5088f2b42859b826ab5faa7f4d283e3b

--- ms1_spectra/corr_cluster.py
+++ ms1_spectra/corr_cluster.py
@@ -18,8 +18,18 @@ class BetaLike(object):
 		self.log_p0_in = np.log(p0_in)
 		self.log_p0_out = np.log(p0_out)
 
-	def comp_like(self,peak1,peak2,adjacency):
-		pass
+	def in_like(self,c):
+		l = np.log(1-self.log_p0_in)
+		l += (self.alp_in - 1)*np.log(c)
+		l += (self.bet_in - 1)*np.log(1-c)
+		return l
+
+
+	def out_like(self,c):
+		l = np.log(1-self.log_p0_out)
+		l += (self.alp_out - 1)*np.log(c)
+		l += (self.bet_out - 1)*np.log(1-c)
+		return l
 
 	def __str__(self):
 		return "Beta"
@@ -33,17 +43,139 @@ class CorrCluster(object):
 		self.alpha = alpha
 		self.N = len(self.peaks)
 		self.base_like()
+		self.init_clusterer()
+
+	class Cluster(object):
+		def __init__(self):
+			self.members = []
+			self.size = 0
+
+	def init_clusterer(self):
+		self.Z = {}
+		self.clusters = []
+		cl = self.Cluster()
+		self.clusters.append(cl)
+		for p in self.peaks:
+			self.Z[p] = cl
+			cl.members.append(p)
+			cl.size += 1
+		print "Initialised with {} clusters".format(len(self.clusters))
 
 	def base_like(self):
 		self.base_like = {}
 
 		for p in self.peaks:
-			like = self.N*like_object.log_p0_out
+			like = self.N*self.like_object.log_p0_out
 			for q in self.adjacency[p]:
-				like += np.log(1-self.like_object.p0_out) + (self.alp_out-1.0)*np.log(adjacency[p][q]) + (self.bet_out-1.0)*np.log(1-adjacency[p][q])
-				like -= selg.log_p0_out
+				like -= self.like_object.log_p0_out
+				like += self.like_object.out_like(self.adjacency[p][q])
 			self.base_like[p] = like
 			
+	def resample_peak_membership(self,peak):
+		current_cluster = self.Z[peak]
+		current_cluster.members.remove(peak)
+		current_cluster.size -= 1
+		if current_cluster.size == 0:
+			self.clusters.remove(current_cluster)
+
+		probs = []
+		max_prob = -1e6
+		for i,cluster in enumerate(self.clusters):
+			pr = np.log(cluster.size) + self.base_like[peak]
+			for p in cluster.members:
+				if p in self.adjacency[peak]:
+					pr += self.like_object.in_like(self.adjacency[peak][p])
+					pr -= self.like_object.out_like(self.adjacency[peak][p])
+				else:
+					pr += self.like_object.log_p0_in
+					pr -= self.like_object.log_p0_out
+			if pr >= max_prob:
+				max_prob = pr
+			probs.append(pr)
+		probs.append(np.log(self.alpha) + self.base_like[peak])
+		if probs[-1] >= max_prob:
+			max_prob = probs[-1]
+
+		probs = np.array(probs)
+		probs = np.exp(probs - max_prob)
+		probs = np.divide(probs,probs.sum()).cumsum()
+
+		new_index = np.where(np.random.rand()<probs)[0][0]
+
+		if new_index < len(self.clusters):
+			new_cluster = self.clusters[new_index]
+			self.Z[peak] = new_cluster
+			new_cluster.members.append(peak)
+			new_cluster.size += 1
+		else:
+			new_cluster = self.Cluster()
+			self.clusters.append(new_cluster)
+			new_cluster.members.append(peak)
+			new_cluster.size += 1
+			self.Z[peak] = new_cluster
+		
+	def check(self):
+		# Check that the numbers tally!
+		print "{} clusters".format(len(self.clusters))
+		n = 0
+		for cluster in self.clusters:
+			n += cluster.size
+		if not n == self.N:
+			print "ERROR"
+
+
+	def gibbs_cycle(self):
+		for peak in self.peaks:
+			self.resample_peak_membership(peak)
+		self.check()
+
+
+	def multi_gibbs_cycle(self,S):
+		for s in range(S):
+			self.gibbs_cycle()
+
+	def get_peaks_by_cluster(self):
+		self.clusters = sorted(self.clusters,key=lambda x: x.size,reverse=True)
+		ordered_peaks = []
+		order = []
+		for cluster in self.clusters:
+			ordered_peaks += cluster.members
+			for p in cluster.members:
+				order.append(self.peaks.index(p))
+		return ordered_peaks,order
+
+
+	def greedy(self,thresh=0.75):
+		# do a greedy clustering
+		self.greedy_clusters = []
+		sorted_peaks = sorted(self.peaks,key=lambda x: x.intensity,reverse=True)
+		finished = False
+		while not finished:
+			if len(sorted_peaks) == 0:
+				finished = True
+				break
+			new_cluster = self.Cluster()
+			p = sorted_peaks[0]
+			new_cluster.members.append(p)
+			new_cluster.size = 1
+			sorted_peaks.remove(p)
+			for q in self.adjacency[p]:
+				if q in sorted_peaks:
+					if self.adjacency[p][q]>=thresh:
+						sorted_peaks.remove(q)
+						new_cluster.members.append(q)
+						new_cluster.size += 1
+			self.greedy_clusters.append(new_cluster)
+
+		self.greedy_clusters = sorted(self.greedy_clusters,key=lambda x: x.size,reverse=True)
+		ordered_peaks = []
+		order = []
+		for cluster in self.greedy_clusters:
+			ordered_peaks += cluster.members
+			for p in cluster.members:
+				order.append(self.peaks.index(p))
+		return ordered_peaks,order
+
 
 	def __str__(self):
 		return "Corr cluster objects with {} peaks and a {} likelihood".format(len(self.peaks),self.like_object)
