bafef2a364d33558cc3015e824e278eeb10f8099
sdrogers <simon.d.rogers@gmail.com>
2015-12-10 16:00:12 +0000

    correlation clustering
    Former-commit-id: 76678d967d7e3164b2808c8c6baf820ebe2afcb3

--- ms1_spectra/corr_cluster.py
+++ ms1_spectra/corr_cluster.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class Peak(object):
 	def __init__(self,pid,mass,rt,intensity):
 		self.pid = pid
@@ -13,6 +15,8 @@ class BetaLike(object):
 		self.bet_out = bet_out
 		self.p0_in = p0_in
 		self.p0_out = p0_out
+		self.log_p0_in = np.log(p0_in)
+		self.log_p0_out = np.log(p0_out)
 
 	def comp_like(self,peak1,peak2,adjacency):
 		pass
@@ -27,7 +31,19 @@ class CorrCluster(object):
 		self.peaks = peaks
 		self.adjacency = adjacency
 		self.alpha = alpha
+		self.N = len(self.peaks)
+		self.base_like()
 
+	def base_like(self):
+		self.base_like = {}
+
+		for p in self.peaks:
+			like = self.N*like_object.log_p0_out
+			for q in self.adjacency[p]:
+				like += np.log(1-self.like_object.p0_out) + (self.alp_out-1.0)*np.log(adjacency[p][q]) + (self.bet_out-1.0)*np.log(1-adjacency[p][q])
+				like -= selg.log_p0_out
+			self.base_like[p] = like
+			
 
 	def __str__(self):
 		return "Corr cluster objects with {} peaks and a {} likelihood".format(len(self.peaks),self.like_object)
