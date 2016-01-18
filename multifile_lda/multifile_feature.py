6e7005d40b1b099107a30e26089fa3195325c253
Joe Wandy <joewandy@gmail.com>
2016-01-18 20:35:29 +0000

    added beer notebook
    Former-commit-id: 6ba58100c72ea764a1aa789489fb02d35aff4992

---
+++ multifile_lda/multifile_feature.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+from scipy.sparse import coo_matrix
+from numpy import int32
+
+class MultifileFeatureExtractor(object):
+
+    def __init__(self):
+        
